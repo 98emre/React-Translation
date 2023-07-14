b7511640596c07a8f742ee43fee060093cc59052
98emre <emre-98@live.se>
2023-07-14 15:50:37 +0200

    feat: added App context that contains the App file

---
+++ translation/src/context/AppContext.jsx
@@ -0,0 +1,15 @@
+import UserProvider from "./UserContext";
+
+
+const AppContext = ({children}) => {
+        return(
+            <>
+                <UserProvider>
+                    {children}
+                </UserProvider>
+            </>
+        )
+}
+
+
+export  default AppContext;
--- translation/src/index.js
+++ translation/src/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import AppContext from './context/AppContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <AppContext>
+        <App/>
+    </AppContext>
 );
 
 
