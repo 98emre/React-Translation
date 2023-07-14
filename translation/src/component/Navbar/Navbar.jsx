ffe50f935d2ff4b34091d26fde288e74b3fba713
98emre <emre-98@live.se>
2023-07-14 16:45:10 +0200

    fix: if user is not logged in they wont see the navbar

--- translation/src/component/Navbar/Navbar.jsx
+++ translation/src/component/Navbar/Navbar.jsx
@@ -1,15 +1,22 @@
 
 
+import { useContext } from "react"
 import { NavLink } from "react-router-dom"
+import { useUser } from "../../context/UserContext"
 
 const Navbar = () => {
+
+    const {user} = useUser()
     
     return (
         <nav>
             <ul>
+                <h1>Translation Page</h1>
+            </ul>
+            {user && <ul>
                 <li> <NavLink to="/">Translate Page</NavLink> </li>
                 <li> <NavLink to="/profile">Profile</NavLink> </li>
-            </ul>
+            </ul>}
         </nav>
     )
 }
