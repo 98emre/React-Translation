1640d5b5fd2387f3f635e2a00d7a686310ac8efb
98emre <emre-98@live.se>
2023-07-14 14:00:40 +0200

    feat: Added router setup in the application where you can route to start page, translation page and profile page

--- translation/src/App.js
+++ translation/src/App.js
@@ -1,11 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Login from "./view/Login"
+import Profile from "./view/Profile"
+import Translation from "./view/Translation"
+
 
 function App() {
-  return (
-    <>
-      <h1>Test</h1>
-      <h3>Test2</h3>
 
-    </>
+  return (    
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/translation" element={<Translation />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/*" element={
+            <>
+              <h1>404 Page Not Found</h1>
+            </>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
