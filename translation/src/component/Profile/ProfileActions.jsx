d77c4b3ae9ec71bf3163e6f0e28dba7f18cbced4
98emre <emre-98@live.se>
2023-07-17 14:32:34 +0200

    feat: added button for logout and history prototype

--- translation/src/component/Profile/ProfileActions.jsx
+++ translation/src/component/Profile/ProfileActions.jsx
@@ -1,12 +1,28 @@
+import { useUser } from "../../context/UserContext"
 
 
 
 
 const ProfileActions = () => {
+
+    const {user } = useUser()
+
+    const handleClearHistoryClick = () => {
+
+    }
+
+    const handleLogoutClick = () => {
+
+    }
     
     return (
         <>
             <p>ProfileActions</p>
+            <ul>
+                <li><button>Clear History</button></li>
+                <li><button>Logout</button></li>
+            </ul>
+
         </>
     )
 }
