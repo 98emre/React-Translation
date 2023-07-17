75d9fd19f7896effd0a541be0dfdbec5fcb84518
98emre <emre-98@live.se>
2023-07-17 15:15:36 +0200

    fix: make sure storage is save in the user context

--- translation/src/context/UserContext.jsx
+++ translation/src/context/UserContext.jsx
@@ -1,4 +1,6 @@
 import { createContext, useContext, useState } from "react";
+import { storageRead } from "../utils/storage";
+import { STORAGE_USER_KEY } from "../utils/storageKey";
 
 
 const UserContext = createContext(null);
@@ -7,7 +9,7 @@ export const useUser = () => useContext(UserContext);
 
 const UserProvider = ({children}) =>{
 
-    const[user, setUser] = useState(null)
+    const[user, setUser] = useState(storageRead(STORAGE_USER_KEY))
 
     const state = {
         user,setUser
