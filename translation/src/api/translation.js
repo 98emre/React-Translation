1071d5a67f50e11d3813e241cb50fa3104cd09fa
98emre <emre-98@live.se>
2023-07-17 15:16:37 +0200

    feat: added api for translation where it handles add translation to the user and  clearing history for the specific user

--- translation/src/api/translation.js
+++ translation/src/api/translation.js
@@ -1,10 +1,49 @@
+import { createHeaders } from "./headerHelper";
+
+const apiURL = process.env.REACT_APP_API_URL;
 
 
-export const addTranslation = async () => {
+export const addTranslation = async (user, translation) => {
     try {
         
-    } catch (error) {
+        const response = await fetch(`${apiURL}/${user.id}`,{
+            method: 'PATCH',
+            headers: createHeaders(),
+            body: JSON.stringify({
+                username: user.username,
+                translations: [...user.translations, translation]
+            })
+        })
+
+
+        if(!response.ok) throw new Error("Could not update the translation")
         
+        const result = await response.json()
+        return [null, result]
+    } catch (error) {
+        return [error.message,null]
+    }
+}
+
+export const translationClearHistory = async (userId) => {
+    try {
+        const response = await fetch(`${apiURL}/${userId}`,{
+            method: 'PATCH',
+            headers: createHeaders(),
+            body: JSON.stringify({
+                translations: []
+            })
+        })
+
+        if(!response.ok){
+            throw new Error("Could note update translations")
+        }
+
+        const result = await response.json()
+        return [null,result];
+
+    } catch (error) {
+        return [error.message,null]
     }
 }
 
