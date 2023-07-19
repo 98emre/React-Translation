ce4dd0b9aa8ef3fc9c1666347ade380f26688feb
98emre <emre-98@live.se>
2023-07-19 15:59:57 +0200

    fix: added navbar with a dropdown

--- translation/src/component/Navbar/Navbar.jsx
+++ translation/src/component/Navbar/Navbar.jsx
@@ -1,22 +1,36 @@
-
-
-import { NavLink } from "react-router-dom"
-import { useUser } from "../../context/UserContext"
+import { useUser } from "../../context/UserContext";
+import { Dropdown } from "react-bootstrap";
+import { NavLink } from "react-router-dom"; // Import NavLink from react-router-dom
 
 const Navbar = () => {
+  const { user } = useUser();
 
-    const {user} = useUser()
-    
-    return (
-        <>
-            <nav>
-            {user && <ul>
-                <li> <NavLink to="/profile">Profile</NavLink> </li>
-                <li> <NavLink to="/translate">Translate</NavLink> </li>
-             </ul>}
-            </nav>
-        </>
-    )
-}
+  return (
+    <>
+      <nav class="navbar navbar-dark bg-dark">
+        {user && (
+          <div class="container">
+            <ul className="navItems">
+              <Dropdown>
+            
+                    <Dropdown.Toggle as={NavLink} className="nav-link navItem">
+                        <h5>
+                            <img src="Logo-Hello.png" width={100} alt="Logo" />
+                            <i className="fas fa-caret-square-down"/>
+                        </h5>
+                    </Dropdown.Toggle>
+                    
+                    <Dropdown.Menu>
+                        <Dropdown.Item href="/profile">View Profile</Dropdown.Item>
+                        <Dropdown.Item href="/translate">Translate</Dropdown.Item>
+                    </Dropdown.Menu>
+              </Dropdown>
+            </ul>
+          </div>
+        )}
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
