690c7c64149d2869c3eedf2091bc2b1aa87d3d7a
98emre <emre-98@live.se>
2023-07-19 14:06:48 +0200

    fix: styling login page with css and chamge style.css file to loginStyle.css file

--- translation/src/view/Login.jsx
+++ translation/src/view/Login.jsx
@@ -4,26 +4,27 @@ const Login = () => {
 
   return (
     <div className="loginContainer">
-      <div className="loginForm">
-        <div className="container">
+      <div className="container">
+        <div className="loginForm">
+
           <div className="row mb-3 justify-content-center align-items-center">
-            <div className="col-auto"></div>
             <div className="col">
-              <h3 className="text-center loginBigText animatedText">Lost in Translation</h3>
+              <h3 className="text-center loginBigText">Lost in Translation</h3>
             </div>
           </div>
 
           <div className="row mb-3 justify-content-center">
-            <h5 className="col-3 text-center">
-              <img src="Logo.png" alt="Icon" width={150} /> <br />
+            <h5 className="col-8 text-center">
+              <img className="loginFormImage shadowContainer" src="Logo.png" alt="Icon" width={150} /> <br />
             </h5>
           </div>
 
           <div className="row mb-3 justify-content-center">
-            <h5 className="col-3 text-center loginSmallText animatedText">Get Started</h5>
+            <div className="col">
+              <h5 className="text-center loginSmallText">Get Started</h5>
+            </div>
           </div>
-
-          <LoginForm />
+         <LoginForm />
         </div>
       </div>
     </div>
